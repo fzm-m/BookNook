17f7a26cfbbfed8bc4e3373da53ff6f980cd60eb
FAN ZHENMING 2302832G <2302832G@student.tp.edu.sg>
2024-12-08 18:59:22 +0800

    Added seed role and user

--- BookNook/Data/BookNookUser.cs
+++ BookNook/Data/BookNookUser.cs
@@ -5,5 +5,8 @@ namespace BookNook.Data
     // Add profile data for application users by adding properties to the ApplicationUser class
     public class BookNookUser : IdentityUser
     {
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+
     }
 }
--- BookNook/Program.cs
+++ BookNook/Program.cs
@@ -34,6 +34,7 @@ builder.Services.AddAuthentication(options =>
     .AddIdentityCookies();
 
 builder.Services.AddIdentityCore<BookNookUser>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddRoles<IdentityRole>()
     .AddEntityFrameworkStores<BookNookContext>()
     .AddSignInManager()
     .AddDefaultTokenProviders();
